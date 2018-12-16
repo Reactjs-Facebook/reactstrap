6e5eac0ef7a79dda749097db345faf39243553c4
Vuk Djukic <vukdukic@gmail.com>
2018-12-16 14:04:56 -0800

    Create headerOriginal.js

---
+++ headerOriginal.js
@@ -0,0 +1,18 @@
+<div className="App">
+      <Header />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://bot.dialogflow.com/65a60d57-1c9b-4bc5-aee2-d9970188b0d4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dialogflow Chat Window
+          </a>
+        </header>
+        <div><Blog /></div>
+        <div><Season /></div>
